2315d755711264cfac15e08c2e04916e05a67b8b
Alex Huang <alexh5@uw.edu>
2020-06-02 16:51:23 -0700

    Set up routes for website

---
+++ website/src/components/HomepageView/HomepageView.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./HomepageView.css";
+
+class HomepageView extends Component {
+
+    render() {
+
+        return (
+            <div>This is the homepage</div>
+        )
+    }
+}
+
+export default HomepageView;
