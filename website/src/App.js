a104661764506cd381d71339625681e58fa75af6
Joseph-K-Tsai <joseph.khamsene.tsai@gmail.com>
2020-06-05 18:23:39 -0700

    Add footer to homepage

--- website/src/App.js
+++ website/src/App.js
@@ -1,30 +1,45 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import HomepageView from "./components/HomepageView/HomepageView.js";
 import BlogHomeView from "./components/BlogHomeView/BlogHomeView.js";
-import ReadPostView from './components/ReadPostView/ReadPostView.js';
-import WritePostView from './components/WritePostView/WritePostView.js';
+import ReadPostView from "./components/ReadPostView/ReadPostView.js";
+import WritePostView from "./components/WritePostView/WritePostView.js";
 import FAQView from "./components/FAQView/FAQView.js";
-import ContactUsView from './components/ContactUsView/ContactUsView';
+import ContactUsView from "./components/ContactUsView/ContactUsView";
+import Footer from "./components/Footer";
 
 class App extends Component {
-
   render() {
-    
     return (
-      <Router>
-        <Route exact path='/' render={() => <HomepageView></HomepageView>} />
-        <Route exact path='/blog' render={() => <BlogHomeView></BlogHomeView>} />
-        <Route path='/blog/read' render={()=> <ReadPostView></ReadPostView>} />
-        <Route path='/blog/write' render={() => <WritePostView></WritePostView>} />
-        <Route path='/faq' render={() => <FAQView></FAQView>} />
-        <Route path='/contactus' render={() => <ContactUsView></ContactUsView>} />
-      </Router>
+      <div>
+        <Router>
+          <Route exact path="/" render={() => <HomepageView></HomepageView>} />
+          <Route
+            exact
+            path="/blog"
+            render={() => <BlogHomeView></BlogHomeView>}
+          />
+          <Route
+            path="/blog/read"
+            render={() => <ReadPostView></ReadPostView>}
+          />
+          <Route
+            path="/blog/write"
+            render={() => <WritePostView></WritePostView>}
+          />
+          <Route path="/faq" render={() => <FAQView></FAQView>} />
+          <Route
+            path="/contactus"
+            render={() => <ContactUsView></ContactUsView>}
+          />
+        </Router>
+        <Footer />
+      </div>
     );
   }
-} 
+}
 
 export default App;
