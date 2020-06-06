ddde595be703a3caf7c15e97115745e8cbe7d929
Joseph-K-Tsai <joseph.khamsene.tsai@gmail.com>
2020-06-05 21:26:17 -0700

    Finalize footer

--- website/src/components/Footer/Footer.js
+++ website/src/components/Footer/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
   render() {
     return (
       <div style={{ backgroundColor: "#f6e173", padding: "40px" }}>
-        <div style={{ float: "left" }}>&#169;2020 New Hope Youth</div>
+        <div style={{ float: "left" }}> &#169;2020 New Hope Youth</div>
         <div style={{ float: "right" }}>Instagram | Youtube</div>
       </div>
     );
