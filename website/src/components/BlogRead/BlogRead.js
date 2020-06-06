89f526ab9189211e857ae368d8e28a6857a6af55
Joseph-K-Tsai <joseph.khamsene.tsai@gmail.com>
2020-06-06 16:01:49 -0700

    Create Blog Read component

--- website/src/components/BlogRead/BlogRead.js
+++ website/src/components/BlogRead/BlogRead.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import './BlogRead.css';
+import React, { Component } from "react";
+import "./BlogRead.css";
+import Card from "react-bootstrap/Card";
 
 class BlogRead extends Component {
   // constructor(props){
-    // super(props);
-    // this.state = {};
+  // super(props);
+  // this.state = {};
   // }
 
   // componentWillMount(){}
@@ -18,7 +19,75 @@ class BlogRead extends Component {
 
   render() {
     return (
-      <div></div>
+      <div>
+        <Card
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginTop: "20px",
+            marginBottom: "20px",
+            width: "80rem",
+            height: "50%",
+          }}
+        >
+          <Card.Body>
+            <div
+              style={{
+                backgroundColor: "gray",
+                paddingLeft: "2%",
+                paddingBottom: "2%",
+              }}
+            >
+              Some image is going to go here
+              <Card.Title
+                style={{
+                  marginTop: "15%",
+                  width: "75%",
+                  height: "100px",
+                  fontSize: "40px",
+                  textAlign: "bottom",
+                }}
+              >
+                [TITLE FROM DB]
+              </Card.Title>
+              <Card.Subtitle className="mb-2">
+                <h2
+                  style={{
+                    width: "15%",
+                    height: "30px",
+                    marginBottom: "5px",
+                    fontSize: "20px",
+                  }}
+                >
+                  [AUTHOR]
+                </h2>
+                <h2
+                  style={{
+                    width: "15%",
+                    height: "30px",
+                    marginBottom: "5px",
+                    fontSize: "20px",
+                  }}
+                >
+                  [DATE]
+                </h2>
+              </Card.Subtitle>
+            </div>
+            <div>
+              <Card.Text
+                style={{
+                  width: "100%",
+                  fontSize: "20px",
+                  padding: "2%",
+                  border: "solid gray 1px",
+                }}
+              >
+                [Content goes here]
+              </Card.Text>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     );
   }
 }
