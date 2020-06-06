14db109eb57f62a2f17d8afdc26ac3ddd37c078c
Joseph-K-Tsai <joseph.khamsene.tsai@gmail.com>
2020-06-06 15:06:48 -0700

    Add BlogWrite component

---
+++ website/src/components/BlogWrite/BlogWrite.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./BlogWrite.css";
+import Card from "react-bootstrap/Card";
+
+class BlogWrite extends Component {
+  // constructor(props){
+  // super(props);
+  // this.state = {};
+  // }
+
+  // componentWillMount(){}
+  // componentDidMount(){}
+  // componentWillUnmount(){}
+
+  // componentWillReceiveProps(){}
+  // shouldComponentUpdate(){}
+  // componentWillUpdate(){}
+  // componentDidUpdate(){}
+
+  render() {
+    return <div></div>;
+  }
+}
+
+export default BlogWrite;
